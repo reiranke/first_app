d1182ef61aeddc68d0bd7e7cd55ad87d343361b8
reiranke <raymond_ranque@yahoo.com>
2013-07-04 19:12:53 +0800

    somthing error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-
-	# def title
-	# base_title ="RUBY SAMPLE APPLICATION"
-	# 	if @title.nil?
-	# 		base_title
-	# 	else
-	# 		"#{base_title} | #{@title}"
-	# 	end
-	# end
 end
