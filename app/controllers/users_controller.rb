2112d6f9e5e089b4da383f02dfc7cddeca40a98c
reiranke <raymond_ranque@yahoo.com>
2013-07-18 16:15:43 +0800

    page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   	 def index
   	 	# @users=User.all
-  	 	@users=User.paginate(:page=>params[:page],:per_page => 20)
+  	 	@users=User.paginate(:page=>params[:page],:per_page => 4)
   	 	@title ="All USER"
   	 end
 
